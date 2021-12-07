deae34f3cd0217125a1726a4682d2cacf9e6b2c5
ericgaspar <junk.eg@free.fr>
2021-12-07 18:48:16 +0100

    Revert "Update _common.sh"
    This reverts commit 5e73cd14c4e4bbbf11398141b9c06e7dba6e24fd.

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ YNH_PHP_VERSION="7.3"
 pkg_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-sqlite3"
 
 # Composer version
-YNH_COMPOSER_VERSION="2.1.3"
+YNH_COMPOSER_VERSION="1.10.17"
 
 #=================================================
 # PERSONAL HELPERS
