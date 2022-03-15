9286c27b98c9c9a3c9259987028dab7e088bf003
yalh76 <yalh@yahoo.com>
2022-03-15 03:20:57 +0100

    Apply example_ynh

--- conf/production.php
+++ conf/production.php
@@ -1,6 +1,6 @@
 <?php return array (
   'app_name' => 'YunoHost Status Page',
-  'app_domain' => 'https://__DOMAIN__',
+  'app_domain' => 'https://__DOMAIN____PATH__',
   'app_timezone' => 'Europe/Paris',
   'app_locale' => '__LANGUAGE__',
   'app_incident_days' => '7',
