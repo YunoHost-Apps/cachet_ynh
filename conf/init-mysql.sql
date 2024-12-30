44b230bec6625e8c7f79df59c94f16164f8f097d
Mateusz <2871798+orhtej2@users.noreply.github.com>
2024-12-30 22:37:18 +0100

    Fix install?

--- conf/init-mysql.sql
+++ conf/init-mysql.sql
@@ -1,10 +1,10 @@
 
-REPLACE INTO `settings` (`group`, `name`, `payload`) VALUES
--- ('app', 'name', 'YunoHost Status Page'),
-('app','domain', 'https://__DOMAIN____PATH__'),
-('app','timezone', 'Europe/Paris'),
-('app','locale', '__LANGUAGE__'),
-('app','incident_days', '7');
+-- REPLACE INTO `settings` (`group`, `name`, `payload`) VALUES
+-- -- ('app', 'name', 'YunoHost Status Page'),
+-- ('app','domain', 'https://__DOMAIN____PATH__'),
+-- ('app','timezone', 'Europe/Paris'),
+-- ('app','locale', '__LANGUAGE__'),
+-- ('app','incident_days', '7');
 
 
 INSERT INTO `users` (`name`, `password`, `email`) VALUES
