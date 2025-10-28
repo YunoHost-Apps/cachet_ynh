923dde29ae9eaa4b0c1863221d03fe091a51be3b
Thomas <51749973+Thovi98@users.noreply.github.com>
2025-10-28 16:37:00 +0100

    lower php version

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,8 +4,6 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-composer_version="2.8.5"
-
 exec_artisan() {
   (cd "$install_dir" && php$php_version artisan $@ --quiet --no-interaction)
 }
