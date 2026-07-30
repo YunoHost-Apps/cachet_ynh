d708c8a6ece31d0e62872449e6d982b39ce07abd
yalh76 <yalh@yahoo.com>
2026-07-30 02:30:28 +0200

    Upgrade to 2026.07.25~ynh1

--- conf/production.php
+++
@@ -1,7 +0,0 @@
-<?php return array (
-  'app_name' => 'YunoHost Status Page',
-  'app_domain' => 'https://__DOMAIN____PATH__',
-  'app_timezone' => 'Europe/Paris',
-  'app_locale' => '__LANGUAGE__',
-  'app_incident_days' => '7',
-);
